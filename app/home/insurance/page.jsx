d80f7dfc0ecb874e0c80f36fccc8660e88f97d81
Adrian <radu.p1995@yahoo.com>
2025-03-23 14:37:19 +0200

    asdsad

--- app/home/insurance/page.jsx
+++ app/home/insurance/page.jsx
@@ -281,13 +281,7 @@ const Insurance = () => {
                     <img src="/images/logo/logo_02.png" alt="logo" width={95} />
                   </Link>
                 </div>
-                {/* <p className="text-white opacity-75 fs-18 mt-15 mb-45 lg-mb-10">
-                  Best Insurance Company.
-                </p>
-                <p className="text-white opacity-50 fs-15 m0 d-none d-lg-block">
-                  © {new Date().getFullYear()} - {new Date().getFullYear() + 1}{" "}
-                  Juridic inc.
-                </p> */}
+               
               </div>
               <Footer />
             </div>
